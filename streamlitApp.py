272d337087e2e8e33354947afe0c558ebd71a0ec
Desmond Lartey <107281765+desmond-lartey@users.noreply.github.com>
2023-08-11 13:35:42 +0200

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -29,19 +29,23 @@ def app():
         attribute_value_dict[attribute] = selected_value
 
     # Button to fetch matching data
-    if st.button("Fetch Matching Data"):
-        mask = True
-        for attr, value in attribute_value_dict.items():
-            mask &= (df[attr] == value)
-        matching_data = df[mask]
-        st.write(matching_data["PlantID"].tolist())
+if selected_attributes and st.button("Fetch Matching Data"):
+    mask = True
+    for attr, value in attribute_value_dict.items():
+        mask &= (df[attr] == value)
+    matching_data = df[mask]
+    st.write(matching_data["PlantID"].tolist())
 
-    # Button to fetch matching plant names (assuming "Plant name" column exists in "plants_corrected.xlsx")
-    if st.button("Fetch Matching Plant Names"):
-        plants_df = pd.read_excel(file_paths["Plants"])
-        matching_plant_ids = df[mask]["PlantID"].tolist()
-        matching_plant_names = plants_df[plants_df["PlantID"].isin(matching_plant_ids)]["Plant name"].tolist()
-        st.write(matching_plant_names)
+# Button to fetch matching plant names (assuming "Plant name" column exists in "plants_corrected.xlsx")
+if selected_attributes and st.button("Fetch Matching Plant Names"):
+    mask = True
+    for attr, value in attribute_value_dict.items():
+        mask &= (df[attr] == value)
+    plants_df = pd.read_excel(file_paths["Plants"])
+    matching_plant_ids = df[mask]["PlantID"].tolist()
+    matching_plant_names = plants_df[plants_df["PlantID"].isin(matching_plant_ids)]["Plant name"].tolist()
+    st.write(matching_plant_names)
+
 
 # Run the app
 app()
