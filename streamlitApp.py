046fa9bdbce1f88f0b8f3b41566b8428201aaf68
Desmond Lartey <107281765+desmond-lartey@users.noreply.github.com>
2023-08-11 15:24:42 +0200

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -27,12 +27,11 @@ def app():
     for attribute in selected_attributes:
         unique_values = df[attribute].dropna().unique().tolist()
         if attribute in ["Climate Zone From", "Climate Zone Till"]:
-            # Assuming 10 climate zones as an example
             selected_values = st.multiselect(f"Select Values for {attribute}:", unique_values)
             selected_values_dict[attribute] = selected_values
         else:
-            selected_value = st.multiselect(f"Select a Value for {attribute}:", unique_values)
-            selected_values_dict[attribute] = selected_value
+            selected_value = st.selectbox(f"Select a Value for {attribute}:", unique_values)
+            selected_values_dict[attribute] = [selected_value]  # Placing it in a list to keep consistent with multiselect
 
     # Further filtering and logic can be added later
 
