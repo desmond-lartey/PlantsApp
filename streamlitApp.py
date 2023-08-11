bf0a10e905276030f38ed7e9f755c6ccb42b8072
Desmond Lartey <107281765+desmond-lartey@users.noreply.github.com>
2023-08-11 14:04:47 +0200

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -23,24 +23,32 @@ def app():
     selected_attributes = st.multiselect("Select Attributes:", df.columns.tolist())
 
     attribute_value_dict = {}
+    mask = True
     for attribute in selected_attributes:
-        unique_values = df[attribute].dropna().unique().tolist()
-        selected_value = st.selectbox(f"Select a Value for {attribute}:", unique_values)
-        attribute_value_dict[attribute] = selected_value
+        if attribute == "ph":
+            range_input = st.text_input(f"Enter {attribute} range (e.g., 5-8 or 5-8,5):")
+            try:
+                parts = range_input.split('-')
+                # Replace comma with dot and convert to float
+                lower_bound = float(parts[0].replace(',', '.'))
+                upper_bound = float(parts[1].replace(',', '.'))
+                mask &= (df[attribute] >= lower_bound) & (df[attribute] <= upper_bound)
+            except ValueError:
+                st.write("Please enter a valid range.")
+                return  # Exit the current execution cycle of the app
+        else:
+            unique_values = df[attribute].dropna().unique().tolist()
+            selected_value = st.selectbox(f"Select a Value for {attribute}:", unique_values)
+            attribute_value_dict[attribute] = selected_value
+            mask &= (df[attribute] == selected_value)
 
     # Button to fetch matching data
     if selected_attributes and st.button("Fetch Matching Data"):
-        mask = True
-        for attr, value in attribute_value_dict.items():
-            mask &= (df[attr] == value)
         matching_data = df[mask]
         st.write(matching_data["PlantID"].tolist())
 
     # Button to fetch matching plant names
     if selected_attributes and st.button("Fetch Matching Plant Names"):
-        mask = True
-        for attr, value in attribute_value_dict.items():
-            mask &= (df[attr] == value)
         plants_df = pd.read_excel(file_paths["Plants"])
         matching_plant_ids = df[mask]["PlantID"].tolist()
         matching_plant_names = plants_df[plants_df["PlantID"].isin(matching_plant_ids)]["Plant name"].tolist()
