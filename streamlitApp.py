cd36e447c2cb06337e9660cc0d20f71d7acd7301
Desmond Lartey <107281765+desmond-lartey@users.noreply.github.com>
2023-08-11 15:38:32 +0200

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -14,25 +14,24 @@ file_paths = {
 
 def app():
     # Dropdown to select a file
-    selected_file = st.selectbox("Select a File:", list(file_paths.keys()))
+    selected_files = st.multiselect("Select Files:", list(file_paths.keys()))
     
-    # Load the selected file
-    df = pd.read_excel(file_paths[selected_file])
-
-    # Dropdown to select an attribute (column name)
-    selected_attributes = st.multiselect("Select Attributes:", df.columns.tolist())
-
     selected_values_dict = {}
+    for selected_file in selected_files:
+        df = pd.read_excel(file_paths[selected_file])
 
-    for attribute in selected_attributes:
-        if attribute in ["climate zone from", "climate zone till"]:
-            # Force the options to be a predefined set of climate zones
-            climate_zones = ["1a", "1b", "2a", "2b", "3a", "3b", "4a", "4b", "5a", "5b", "6a", "6b", "7a", "7b", "8a", "8b", "9a", "9b", "10a", "10b", "11a", "11b", "12a", "12b", "13a", "13b", "14a", "14b"]
-            selected_values = st.multiselect(f"Select Values for {attribute}:", climate_zones)
-        else:
-            unique_values = sorted(df[attribute].dropna().unique().tolist())
-            selected_values = st.multiselect(f"Select Values for {attribute}:", unique_values)
-        selected_values_dict[attribute] = selected_values
+        # Dropdown to select an attribute (column name)
+        selected_attributes = st.multiselect(f"Select Attributes for {selected_file}:", df.columns.tolist(), key=selected_file)
+
+        for attribute in selected_attributes:
+            if attribute in ["climate zone from", "climate zone till"]:
+                # Force the options to be a predefined set of climate zones
+                climate_zones = ["1a", "1b", "2a", "2b", "3a", "3b", "4a", "4b", "5a", "5b", "6a", "6b", "7a", "7b", "8a", "8b", "9a", "9b", "10a", "10b", "11a", "11b", "12a", "12b", "13a", "13b", "14a", "14b"]
+                selected_values = st.multiselect(f"Select Values for {attribute} in {selected_file}:", climate_zones, key=f"{selected_file}_{attribute}")
+            else:
+                unique_values = sorted(df[attribute].dropna().unique().tolist())
+                selected_values = st.multiselect(f"Select Values for {attribute} in {selected_file}:", unique_values, key=f"{selected_file}_{attribute}")
+            selected_values_dict[f"{selected_file}_{attribute}"] = selected_values
 
     # This is where you can add further logic and filtering based on the selected values
 
