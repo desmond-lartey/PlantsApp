b5d547c9edf72ebba1330572ee479bc3161e5fa3
Desmond Lartey <107281765+desmond-lartey@users.noreply.github.com>
2023-08-13 20:41:51 +0200

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -35,6 +35,9 @@ def landing_page():
     st.write("[Go to main project page](#)")  # Replace # with the actual link to your main project page
     st.write("[Visit Agro-NL Consult SolutionS B.V](https://agro-nl.nl/)")
 
+def app():
+    landing_page()
+    
     # Dropdown to select a file
     selected_files = st.multiselect("Select Files:", list(file_paths.keys()))
     
@@ -46,21 +49,21 @@ def landing_page():
 
         for attribute in selected_attributes:
             if selected_file == 'Climate' and attribute == 'pH':
-    ph_options = [str(i) for i in range(1, 15)] + [f"{i}-{i+2}" for i in range(1, 13)]
-    selected_ph = st.selectbox('Select pH value or range:', ph_options)
+                ph_options = [str(i) for i in range(1, 15)] + [f"{i}-{i+2}" for i in range(1, 13)]
+                selected_ph = st.selectbox('Select pH value or range:', ph_options)
 
-    if '-' in selected_ph:
-        ph_start, ph_end = map(int, selected_ph.split('-'))
-        ph_values = list(range(ph_start, ph_end + 1))
-    else:
-        ph_value = int(selected_ph)
-        ph_values = list(range(1, ph_value + 1))
+                if '-' in selected_ph:
+                    ph_start, ph_end = map(int, selected_ph.split('-'))
+                    ph_values = list(range(ph_start, ph_end + 1))
+                else:
+                    ph_value = int(selected_ph)
+                    ph_values = list(range(1, ph_value + 1))
                 
-    selected_values_dict[(selected_file, attribute)] = ph_values
-else:
-    unique_values = sorted(df[attribute].dropna().unique().tolist())
-    selected_values = st.multiselect(f"Select Values for {attribute} in {selected_file}:", unique_values, key=f"{selected_file}_{attribute}")
-    selected_values_dict[(selected_file, attribute)] = selected_values
+                selected_values_dict[(selected_file, attribute)] = ph_values
+            else:
+                unique_values = sorted(df[attribute].dropna().unique().tolist())
+                selected_values = st.multiselect(f"Select Values for {attribute} in {selected_file}:", unique_values, key=f"{selected_file}_{attribute}")
+                selected_values_dict[(selected_file, attribute)] = selected_values
 
     # Fetch matching plant names
     if st.button("Fetch Matching Plant Names"):
