d7b5eb268da50b3c49157cba88a1d6a9a37b3aba
Desmond Lartey <107281765+desmond-lartey@users.noreply.github.com>
2023-10-10 19:56:48 +0200

    editied

--- streamlitApp2.py
+++ streamlitApp2.py
@@ -181,6 +181,8 @@ def modified_app():
         st.write("Matching Plant Names:")
         st.write(matching_plant_names)
 
+if st.button("Show Map"):
+    map_app()
     # Create some space using CSS padding
     st.markdown('<div style="padding: 50px;"></div>', unsafe_allow_html=True)
 
