083f7f9d0b6af10549c8c3d8e04f7734969e4b1c
Desmond Lartey <107281765+desmond-lartey@users.noreply.github.com>
2023-10-10 17:09:59 +0200

    edited

--- map_interface.py
+++ map_interface.py
@@ -5,8 +5,9 @@ def app():
     st.title("Home")
     st.markdown(
         """
-    A [streamlit](https://streamlit.io) app template for geospatial applications. 
+    A [streamlit](https://streamlit.io) app template for geospatial applications based on [streamlit-option-menu](https://github.com/victoryhb/streamlit-option-menu). 
     To create a direct link to a pre-selected menu, add `?page=<app name>` to the URL, e.g., `?page=upload`.
+    https://share.streamlit.io/giswqs/streamlit-template?page=upload
     """
     )
 
